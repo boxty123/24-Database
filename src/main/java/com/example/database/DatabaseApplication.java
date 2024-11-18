48355d07cfb619c51eea276d82c552cc0614a587
JungHyunLee <aktlakfhjh2@gmail.com>
2024-11-18 11:29:21 +0900

    CRUD

---
+++ src/main/java/com/example/database/DatabaseApplication.java
@@ -0,0 +1,55 @@
+package com.example.database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@SpringBootApplication()
+public class DatabaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseApplication.class, args);
+
+        try {
+            Connection con = DriverManager.getConnection(
+                    "jdbc:mysql://192.168.111.131:4567/madang",
+                    "leejeonghyeon", "password");
+            Statement stmt = con.createStatement();
+
+
+//            String insertQuery = "INSERT INTO Book(bookid, bookname, publisher, price) " +
+//                    "VALUES (18,'이정현 공부법', '정현출판사', 13000)";
+//            stmt.executeUpdate(insertQuery);
+
+
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Book");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) +
+                        "  " + rs.getString(3)+" "+rs.getInt(4));
+
+
+            String deleteQuery = "DELETE FROM Book WHERE bookid = 22";
+            stmt.executeUpdate(deleteQuery);
+
+            System.out.println("\n");
+
+            rs = stmt.executeQuery("SELECT * FROM Book");
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) +
+                        "  " + rs.getString(3)+" "+rs.getInt(4));
+
+
+            con.close();
+
+
+
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
